86457117c282204d2eca8ba8a430d1c44df9ab99
starkshaw <ttxzb@hotmail.com>
2015-01-26 21:44:05 +0000

    Modified copyright info

--- FindPrimeNumber/FindPrimeNumber/Properties/AssemblyInfo.cs
+++ FindPrimeNumber/FindPrimeNumber/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FindPrimeNumber")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Zhenbang Xiao 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
