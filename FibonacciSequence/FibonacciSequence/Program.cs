d42e2e3bb57e90476990120a5ea99875158db7f9
starkshaw <ttxzb@hotmail.com>
2015-01-27 00:00:55 +0000

    Modified grammar

--- FibonacciSequence/FibonacciSequence/Program.cs
+++ FibonacciSequence/FibonacciSequence/Program.cs
@@ -11,26 +11,26 @@ namespace Fibonacci {
 				Console.Write("Enter the index of Fibonacci number: ");
 				n = int.Parse(Console.ReadLine());
 				Console.WriteLine(fibGetIndex(n));
-			}else if (args[0]=="-list") {
-			try {
-				printFibList(int.Parse(args[1]));
-			} catch (FormatException) {
-				Console.WriteLine("Argument: " + args[1] + " must be an integer.");
-				Environment.Exit(1);
+			} else if (args[0] == "-list") {
+				try {
+					printFibList(int.Parse(args[1]));
+				} catch (FormatException) {
+					Console.WriteLine("Argument: {0} must be an integer.", args[1]);
+					Environment.Exit(1);
+				}
+			} else if (args[0] == "-index") {
+				try {
+					Console.WriteLine(fibGetIndex(int.Parse(args[1])));
+				} catch (FormatException) {
+					Console.WriteLine("Argument: {0} must be an integer.", args[1]);
+					Environment.Exit(1);
+				}
+			} else {
+				Console.WriteLine("\nDetermine the Fibonacci sequence.\n   FibonacciSequence ([-index|-list] indexNumber) | [-help]");
+				Console.WriteLine("\n\t-index indexNumber     Print out the indexNumber-th Fibonacci number.");
+				Console.WriteLine("\n\t-list indexNumber      Print out the whole list of Fibonacci numbers less than the content of indexNumber.");
+				Console.WriteLine("\n\t-help                  Print out this help screen.\n");
 			}
-		} else if (args[0]=="-index") {
-			try {
-				Console.WriteLine(fibGetIndex(int.Parse(args[1])));
-			} catch (FormatException) {
-				Console.WriteLine("Argument: " + args[1] + " must be an integer.");
-				Environment.Exit(1);
-			}
-		} else {
-			Console.WriteLine("\nDetermine the Fibonacci sequence.\n   FibonacciSequence ([-index|-list] indexNumber) | [-help]");
-			Console.WriteLine("\n\t-index indexNumber     Print out the indexNumber-th Fibonacci number.");
-			Console.WriteLine("\n\t-list indexNumber      Print out the whole list of Fibonacci numbers less than the content of indexNumber.");
-			Console.WriteLine("\n\t-help                  Print out this help screen.\n");
-		}
 		}
 
 		public static long fibGetIndex(int n) {
@@ -47,7 +47,7 @@ namespace Fibonacci {
 			Console.ForegroundColor = ConsoleColor.Red;
 			for (int i = 1; i <= n; i++) {
 				if (i % 10 != 0) {
-					Console.Write("{0} ",fibGetIndex(i));
+					Console.Write("{0} ", fibGetIndex(i));
 				} else {
 					Console.WriteLine(fibGetIndex(i));
 				}
