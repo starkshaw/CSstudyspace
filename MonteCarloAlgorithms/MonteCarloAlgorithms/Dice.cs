de69bdc005664a347b1bca02ebf101af8cb2dd16
starkshaw <ttxzb@hotmail.com>
2015-01-28 19:26:05 +0000

    Add Monte Carlo Algorithms in C#

---
+++ MonteCarloAlgorithms/MonteCarloAlgorithms/Dice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonteCarloAlgorithms {
+	class Dice {
+		private static Random ran = new Random();
+		private int point;
+		public Dice()
+			: base() {
+		}
+
+		public void roll() {
+			//this.point = (int)(ran.Next(0, 100) % 6) + 1;
+			this.point = ran.Next(1, 7);			// Those two methods have different behaviors.
+		}
+
+		// Getter and Setter
+		public int Point {
+			get { return this.point; }
+			set { this.point = value; }
+		}
+
+	}
+}
---
+++ MonteCarloAlgorithms/MonteCarloAlgorithms/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonteCarloAlgorithms {
+	class Program {
+		static void Main(string[] args) {
+			Dice[] obj = new Dice[5];
+			int[] result = new int[30];
+			for (int i = 0; i < result.GetLength(0); i++) {
+				result[i] = 0;
+			}
+			for (int i = 0; i <= 4; i++) {
+				obj[i] = new Dice();
+			}
+			for (int i = 0; i < 1000000; i++) {
+				int sum = 0;
+				for (int j = 0; j < obj.GetLength(0); j++) {
+					obj[j].roll();
+					//Console.WriteLine("Value\t{0}",obj[j].Point);
+					sum = sum + obj[j].Point;
+				}
+				//Console.WriteLine("Sum\t{0}",sum);
+				result[sum - 1] += 1;
+			}
+			for (int i = 0; i < result.GetLength(0); i++) {
+				Console.WriteLine("{0}\t{1}", i + 1, result[i]);
+			}
+		}
+	}
+}
