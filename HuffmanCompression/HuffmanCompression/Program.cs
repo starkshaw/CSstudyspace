522c01dad2b934c38507da675be66975c0bab9fe
starkshaw <ttxzb@hotmail.com>
2015-02-20 16:20:12 +0000

    Add Huffman Compression

---
+++ HuffmanCompression/HuffmanCompression/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HuffmanCompression {
+	class Program {
+		static void Main(string[] args) {
+			Console.Write("Enter a string: ");
+			string str = Console.ReadLine();
+			Console.WriteLine("The ASCII code are: {0}",string.Join(", ", Encoding.ASCII.GetBytes(str)));
+		}
+	}
+}
