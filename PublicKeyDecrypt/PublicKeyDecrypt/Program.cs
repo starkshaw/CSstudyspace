7549bf58352f30fd8f9e9dc28427df9a6496c70b
starkshaw <ttxzb@hotmail.com>
2015-03-30 09:30:36 +0100

    Added documents of method and exception handlers.

--- PublicKeyDecrypt/PublicKeyDecrypt/Program.cs
+++ PublicKeyDecrypt/PublicKeyDecrypt/Program.cs
@@ -6,20 +6,27 @@ using System.Text;
 namespace PublicKeyDecrypt {
 	class Program {
 		static void Main(string[] args) {
+			//Console.WriteLine(modPow(7, 31, 11));
+			//Console.WriteLine(modMult(7, 88, 9));
+
+			// Initialize
 			long[] publicKey = new long[3];
-			Console.WriteLine("Enter the public key consists by 3 integers, use comma separate only.\n");
+			Console.WriteLine("\nEnter the public key consists by 3 integers, use comma separate only.\n");
 			Console.Write("Public Key: ");
-			string[] input = Console.ReadLine().Split(',');
 			try {
+				string[] input = Console.ReadLine().Split(',');
 				for (int i = 0; i < input.Length; i++) {
 					publicKey[i] = long.Parse(input[i]);
 				}
-			} catch (System.IndexOutOfRangeException) {
-				Console.WriteLine("\nERROR: Overloaded carries: {0}", input.Length);
+			} catch (System.IndexOutOfRangeException) {		// Exception handlers
+				Console.WriteLine("\nERROR: Overloaded carries.");
 				Console.WriteLine("The public key shall only contains 3 integers.");
 			} catch (System.FormatException) {
 				Console.WriteLine("\nERROR: Wrong data type.");
 				Console.WriteLine("The public key shall only contains 3 integers.");
+			} catch (System.NullReferenceException) {
+				Console.WriteLine("\nAbort.");
+				Environment.Exit(0);
 			} finally {
 				Environment.Exit(0);
 			}
@@ -27,7 +34,7 @@ namespace PublicKeyDecrypt {
 
 		/// <summary>
 		/// Find the modulus from this form:
-		/// number^power mod modulus
+		/// number ^ power mod modulus
 		/// </summary>
 		/// <param name="number">The base number</param>
 		/// <param name="power">The power of base number</param>
@@ -38,7 +45,7 @@ namespace PublicKeyDecrypt {
 			//when raising a number to a power, the number quickly becomes too large to
 			//handle
 			//you need to multiply numbers in such a way that the result is consistently
-			//moduloed to keep it in the range
+			//modulo-ed to keep it in the range
 			//however you want the algorithm to work quickly - having a multiplication
 			//loop would result in an O(n) algorithm!
 			//the trick is to use recursion - keep breaking the problem down into smaller
@@ -56,18 +63,19 @@ namespace PublicKeyDecrypt {
 		}
 
 		/// <summary>
-		/// 
+		/// Find the modulus from this form:
+		/// first * second mod modulus
 		/// </summary>
-		/// <param name="first"></param>
-		/// <param name="second"></param>
-		/// <param name="modulus"></param>
-		/// <returns></returns>
+		/// <param name="first">The first number</param>
+		/// <param name="second">The second number</param>
+		/// <param name="modulus">The modulus value</param>
+		/// <returns>The modules</returns>
 		public static long modMult(long first, long second, long modulus) {
 			//multiplies the first number by the second number with the given modulus
 			//a long can have a maximum of 19 digits. Therefore, if you're multiplying
 			//two ten digits numbers the usual way, things will go wrong
 			//you need to multiply numbers in such a way that the result is consistently
-			//moduloed to keep it in the range
+			//modulo-ed to keep it in the range
 			//however you want the algorithm to work quickly - having an addition loop
 			//would result in an O(n) algorithm!
 			//the trick is to use recursion - keep breaking down the multiplication into
