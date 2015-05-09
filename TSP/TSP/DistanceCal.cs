76cf4be545212296c48964b40b8b2fc05266a032
starkshaw <ttxzb@hotmail.com>
2015-05-09 16:26:47 +0100

    Added Traveling Salesman Problem.

---
+++ TSP/TSP/DistanceCal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TSP {
+	public static class DistanceCal {
+
+		public const double RADIUS = 6371;               // The mean radius of earth
+
+		public static double calDistance(double lat1, double lon1, double lat2, double lon2) {
+			double dlon = ToRadians(lon2 - lon1);   // Delta longitude
+			double dlat = ToRadians(lat2 - lat1);   // Delta latitude
+
+			double a = Math.Sin(dlat / 2) * Math.Sin(dlat / 2)
+					+ Math.Cos(ToRadians(lat1)) * Math.Cos(ToRadians(lat2))
+					* Math.Sin(dlon / 2) * Math.Sin(dlon / 2);
+
+			double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+			return RADIUS * c;
+		}
+
+		public static double ToRadians(this double val) {
+			return (Math.PI / 180) * val;
+		}
+
+		public static double CalDistanceByIndex(int index1, int index2, double[,] coordinates) {
+			return DistanceCal.calDistance(
+					coordinates[index1,0],
+					coordinates[index1,1],
+					coordinates[index2,0],
+					coordinates[index2,1]);
+		}
+
+	}
+}
---
+++ TSP/TSP/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+
+namespace TSP {
+	class Program {
+
+		public static string currentPath = Directory.GetCurrentDirectory();
+		public static string filePath = string.Format(@"{0}//alltowns.txt", currentPath);
+
+		static void Main(string[] args) {
+			Console.Write("Loading file... ");
+			if (File.Exists(filePath)) {
+				Console.WriteLine("Finished. Organizing... ");
+				string[] rawData = File.ReadAllLines(filePath);
+				string[,] allData = new String[rawData.Length, 4];
+				for (int i = 0; i < allData.GetLength(0); i++) {
+					string[] tmp = rawData[i].Split(',');
+					for (int j = 0; j < allData.GetLength(1); j++) {
+						allData[i, j] = tmp[j];
+					}
+				}
+				double[,] coordinates = new double[allData.GetLength(0), 2];
+				for (int i = 0; i < coordinates.GetLength(0); i++) {
+					coordinates[i, 0] = double.Parse(allData[i, 2]);
+					coordinates[i, 1] = double.Parse(allData[i, 3]);
+				}
+				Console.WriteLine("Finished.\nCalculating... ");
+				double[,] distanceTable = new double[allData.GetLength(0), allData.GetLength(0)];
+				for (int i = 0; i < distanceTable.GetLength(0); i++) {
+					for (int j = 0; j < distanceTable.GetLength(1); j++) {
+						distanceTable[i, j] = DistanceCal.CalDistanceByIndex(i, j, coordinates);
+						Console.Write(Math.Round(distanceTable[i, j], 0) + "\t");
+					}
+					Console.WriteLine();
+				}
+				for (int i = 0; i < allData.GetLength(0); i++) {
+					ArrayList unvisitedList = new ArrayList();
+					for (int j = 0; i < allData.GetLength(0); j++) {
+						unvisitedList.Add(i);
+					}
+					while (unvisitedList.Count != 0) {
+						
+					}
+				}
+			} else {
+				Console.WriteLine("Failed.\nFile {0} does not exist.", filePath);
+				Environment.Exit(-1);
+			}
+
+
+		}
+	}
+}
